3cbdf3d8a27eac5f06bc83b489a447234a7d948e
Alex Alvarado <Alexjalva@gmail.com>
2021-01-07 23:25:50 -0800

    pushing through final code changes, fully functional code

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,73 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var passwordReqs = {};
-  passwordReqs = passReqs(passwordReqs);
-  console.log(passwordReqs);
-  var password = generatePassword(passwordReqs);
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+//Validate user number input
+function checkLenInput(pLen){
+  if(parseInt(pLen) > 7 && parseInt(pLen) <129){
+    return false;
+  }
+  else{
+    return true;
+  }
 }
+
+//validate user yes or no input
+function checkYesNoInput(response){
+  let input = response.trim();
+  if(input.toLowerCase() == "yes" || input.toLowerCase() == "no"){
+    return false;
+  }
+  else{
+    alert("Please answer with yes or no")
+    return true;
+  }
+}
+
 //Get user requirements function
 function passReqs(reqsDict){
   reqsDict = {
     passLen: null,
-    charTypes: null,
+    charTypes: [false, false, false, false],
   }
-  reqsDict.charTypes = [true, true, true, true]
-  reqsDict.passLen = 10;
+  while(checkLenInput(reqsDict.passLen) ){
+    alert("Password must be at least 8 characters long, and no longer than 128 characters. Please input only numbers.");
+    reqsDict.passLen = prompt("Enter the desired length of your password.")
+  }
+  while(reqsDict.charTypes[0] == false && reqsDict.charTypes[1] == false && reqsDict.charTypes[2] == false && reqsDict.charTypes[3] == false){
+    alert("Please respond to the following with 'yes' or 'no'. The following prompts will ask if you would like: lower case letters, upper case letters, numbers, and special characters. You will need to answer yes to at least one.");
+    let userResponse = prompt("Would you like lower case letters in your password? (yes/no)");
+    while(checkYesNoInput(userResponse)){
+      userResponse = prompt("Would you like lower case letters in your password? (yes/no)");
+    }
+    userResponse= userResponse.trim();
+    if(userResponse.toLowerCase() == "yes"){
+      reqsDict.charTypes[0] = true;
+    }
+    userResponse = prompt("Would you like upper case letters in your password? (yes/no)");
+    while(checkYesNoInput(userResponse)){
+      userResponse = prompt("Would you like upper case letters in your password? (yes/no)");
+    }
+    userResponse= userResponse.trim();
+    if(userResponse.toLowerCase() == "yes"){
+      reqsDict.charTypes[1] = true;
+    }
+    userResponse = prompt("Would you like numbers in your password? (yes/no)");
+    while(checkYesNoInput(userResponse)){
+      userResponse = prompt("Would you like numbers in your password? (yes/no)");
+    }
+    userResponse= userResponse.trim();
+    if(userResponse.toLowerCase() == "yes"){
+      reqsDict.charTypes[2] = true;
+    }
+    userResponse = prompt("Would you like special characters in your password? (yes/no)");
+    while(checkYesNoInput(userResponse)){
+      userResponse = prompt("Would you like special characters in your password? (yes/no)");
+    }
+    userResponse= userResponse.trim();
+    if(userResponse.toLowerCase() == "yes"){
+      reqsDict.charTypes[3] = true;
+    }
+  }
   return reqsDict;
 }
 
@@ -30,7 +78,7 @@ function generatePassword(pReqs){
     passOptions.push("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
   }
   if(pReqs.charTypes[1]){
-    passOptions.push("A","B","C","D","E","F","G","H","I","J","K","L","m","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+    passOptions.push("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
   }
   if(pReqs.charTypes[2]){
     passOptions.push("1","2","3","4","5","6","7","8","9","0");
@@ -44,6 +92,15 @@ function generatePassword(pReqs){
     passwordString += passOptions[randomNum];
   }
   return passwordString;
+}
+
+// Write password to the #password input
+function writePassword() {
+  var passwordReqs = {};
+  passwordReqs = passReqs(passwordReqs);
+  var password = generatePassword(passwordReqs);
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
 }
 
 // Add event listener to generate button
